ca90e7b7a0207a9e6e1898f110f11c29aeb11cca
Aminul Islam <aminulds61@gmail.com>
2021-10-07 22:03:38 +0600

    added practive problem

--- problemSolving/uriOnlineJudge/1015.js
+++ problemSolving/uriOnlineJudge/1015.js
@@ -1,15 +1,13 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-var rowSpace = lines.shift().split(' ');
+// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+// var lines = input.split('\n');
+// var rowSpace = lines.shift().split(' ');
 
-var x1 = parseFloat(rowSpace.shift());
-var x2 = parseFloat(lines.shift());
+var x1 = parseFloat(1.0);
+var x2 = parseFloat(7.0);
 
-var y1 = parseFloat(rowSpace.shift());
-var y2 = parseFloat(lines.shift());
-
+var y1 = parseFloat(5.0);
+var y2 = parseFloat(9.0);
 var x = Math.pow((x2 - x1), 2);
 var y = Math.pow((y2 - y1), 2);
-var distance = x + y;
-distance = Math.sqrt(distance).toFixed(4);
-console.log(distance);
+
+console.log(Math.sqrt(x + y));
